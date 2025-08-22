d6df74862e8f1ca0051a5de9e65fab0083cc3c3d
Viktor Szabo <szabo_viktor@lilly.com>
2025-08-22 09:08:34 -0400

    Improve documentation formatting in load_data function for better readability. Refactor code to enhance clarity in argument handling and loading process.

--- R/load_data.R
+++ R/load_data.R
@@ -1,18 +1,24 @@
 #' @title Load Data
 #'
 #' @description
-#' Loads and returns a specified dataset from the data registry in the given execution environment.
+#' Loads and returns a specified dataset from the data registry in the given
+#' execution environment.
 #'
 #' @details
-#' This function retrieves the data registry from the provided execution environment. It checks if the specified dataset
-#' exists in the registry and if the path to the data is available. If the dataset or path is missing, an error is raised.
-#' The function then loads the dataset using the specified data loader based on the data type defined in the registry.
-#' Messages are displayed indicating the successful loading of the dataset along with its type and loading method.
+#' This function retrieves the data registry from the provided execution
+#' environment. It checks if the specified dataset exists in the registry and
+#' if the path to the data is available. If the dataset or path is missing, an
+#' error is raised. The function then loads the dataset using the specified
+#' data loader based on the data type defined in the registry. Messages are
+#' displayed indicating the successful loading of the dataset along with its
+#' type and loading method.
 #'
-#' @param name The name of the dataset to be loaded, as specified in the data registry.
+#' @param name The name of the dataset to be loaded, as specified in the data
+#' registry.
 #' @param execution The execution environment containing the data registry.
 #'
-#' @return Returns the loaded dataset, and it is also stored in the execution environment for further use.
+#' @return Returns the loaded dataset, and it is also stored in the execution
+#' environment for further use.
 #'
 load_data <- function(name, execution) {
   ## Get data registry from execution environment
@@ -57,7 +63,13 @@ load_data <- function(name, execution) {
 
   loader <- eval(parse(text = loader_func))
   message("(bro) Loading '", name, "' (", type, ", ", loader_func, ")")
-  execution$data[[name]] <- do.call(what = loader, args = append(list(file = path), registry[[name]]$load_args))
+  execution$data[[name]] <- do.call(
+    what = loader,
+    args = append(
+      list(file = path),
+      registry[[name]]$load_args
+    )
+  )
 
-  return(execution$data[[name]])
+  execution$data[[name]]
 }
