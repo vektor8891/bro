060cd28b5d276ce7767e450334d2a19d4223c7f8
Viktor Szabo <szabo_viktor@lilly.com>
2025-08-22 09:54:27 -0400

    Enhance documentation for the .onLoad function in init.R, adding detailed descriptions and parameters for improved clarity. Refactor the loading of the connectors file to streamline the code and maintain consistency with existing practices.

--- R/init.R
+++ R/init.R
@@ -1,13 +1,18 @@
-##' @title List of functions that can be used to load and save different types
-##' of data
-##' @author Guilherme Salome
-##' @export
+#' @title List of functions that can be used to load and save different types
+#' of data
+#' @author Guilherme Salome
+#' @export
 connectors <- NULL
 
-
+#' @title Load connectors file
+#' @description Loads the connectors file from the package
+#' @param libname The library name
+#' @param pkgname The package name
+#' @importFrom yaml read_yaml
+#' @keywords internal
 .onLoad <- function(libname, pkgname) {
-  ## Load connectors file
-  connectors <<- yaml::read_yaml(system.file("connectors.yaml",
-    package = pkgname
-  ))
+  # Load connectors file
+  connectors <<- read_yaml( # nolint: object_usage_linter
+    system.file("connectors.yaml", package = pkgname)
+  )
 }
