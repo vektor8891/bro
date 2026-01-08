6cdfbcff5c95286cb5e932fc5cca70b436afb40d
Viktor Szabo <szabo_viktor@lilly.com>
2026-01-08 13:33:28 -0500

    Add var_exists function to check variable existence in memory or disk

---
+++ R/var_exists.R
@@ -0,0 +1,49 @@
+#' Checks if a variable exists in memory or disk.
+#'
+#' Given the name of a variable, check if it exists in memory or in disk (local
+#' or remote).
+#' @param output (string) Name of a variable.
+#' @param context (environment) Environment containing data catalog.
+#' @param parameters (string, optional) Name of a parameters YML file stored in
+#' the conf/base folder.
+#' @return (boolean) TRUE if variable exists in memory or disk.
+var_exists <- function(output, context, parameters = "parameters.yml") {
+  ## In memory
+  if (base::exists(output, where = context, inherits = FALSE)) {
+    return(TRUE)
+  }
+  ## In disk (local or remote)
+  if (output %in% names(context$catalog)) {
+    vartype <- context$catalog[[output]]$type
+    filepath_raw <- context$catalog[[output]]$filepath
+    filepath <- glue::glue(filepath_raw, .envir = context)
+    if (length(filepath) == 0) {
+      stop(paste0("Couldn't evaluate filepath for ", output, ". Stop."))
+    }
+    if (framebar:::is_remote(vartype)) {
+      remote_table <- stringr::str_split(filepath, "\\.")[[1]]
+      connection <- framebar:::get_connection(vartype, context)
+      return(DBI::dbExistsTable(connection, DBI::Id(
+        schema = remote_table[1],
+        table = remote_table[2]
+      )))
+    } else { # local files are potentially versioned
+      versioned <- context$catalog[[output]]$versioned
+      if (!is.null(versioned) && versioned == TRUE) {
+        filepath <- framebar:::load_versioned(vartype, filepath, context)
+      }
+      # include parameters folder if file path is not in 01_raw
+      if (!base::grepl(
+        "01_raw", filepath,
+        fixed = TRUE
+      ) && parameters != "parameters.yml") {
+        filepath <- base::file.path(
+          base::dirname(filepath), parameters,
+          base::basename(filepath)
+        )
+      }
+      return(base::file.exists(filepath))
+    }
+  }
+  return(FALSE)
+}
